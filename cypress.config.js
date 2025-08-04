ebf7112ce9bcf5fd81a8e1107a7673f4500e3aa0
bobwiths <marcelosachett@gmail.com>
2025-08-04 19:46:41 -0300

    fix CI components

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: "https://front.serverest.dev/admin/home",
+    supportFile: "cypress/support/e2e.js"
   },
 });
